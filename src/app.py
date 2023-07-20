03b7f18c0a892cbaaebfef798beb7721af2af8c7
KosPsych <gidabrasti999@hotmail.com>
2023-07-20 15:33:20 +0300

    added healthcheck

--- src/app.py
+++ src/app.py
@@ -1,10 +1,18 @@
 import uvicorn
 from fastapi import FastAPI, Request
 from endpoints.feature_engineering import *
+from endpoints.healthcheck import *
 app = FastAPI()
 
 # curl -X POST http://127.0.0.1:8000/feature_engineering -H "Content-Type: application/json" -d @cvas_data.json
 
+
+# Feature engineering
 @app.post("/feature_engineering")
 async def create_features(request: Request):
     return feature_engineering (await request.json())
+
+# simple healthcheck Endpoint
+@app.get("/healthcheck")
+def healthcheck():
+    return {'status': 'UP'}
